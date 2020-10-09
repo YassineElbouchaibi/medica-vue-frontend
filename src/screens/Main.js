f4c31b7ccbee504cfc556783bc117906355e23e2
Yassine El Bouchaibi <yassineelbouchaibi@polymtl.ca>
2020-10-08 21:48:35 -0400

    Paper

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -3,20 +3,21 @@ import CornerstoneViewport from 'react-cornerstone-viewport'
 import { useRecoilState } from 'recoil';
 import './Main.css';
 import { todoListState } from '../state/Cornerstone/tools'
+import { Paper } from '@material-ui/core';
 
 function Main() {
     const [tools, setTools] = useRecoilState(todoListState)
 
     return (
         <div className="Main">
-            <div className="CornerstoneContainer">
+            <Paper className="CornerstoneContainer" elevation={2}>
                 <CornerstoneViewport
                     tools={tools}
                     imageIds={[
                         'https://cdn.jpegmini.com/user/images/slider_puffin_jpegmini.jpg',
                     ]}
                 />
-            </div>
+            </Paper>
         </div>
     );
 }
